62adce526535a908076fdd4a6a0a4fdab40557c1
Cameron Lockhart <cameronl.lockhart@gmail.com>
2017-05-15 18:00:41 -0400

    Updated mix.exs for inclusion in hex.pm

--- mix.exs
+++ mix.exs
@@ -3,7 +3,9 @@ defmodule Urlpreview.Mixfile do
 
   def project do
     [app: :urlpreview,
-     version: "0.1.0",
+     version: "0.0.1",
+     description: "Fetches meta data from websites and returns information useful for things such as URL previews.",
+     package: package(),
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -31,5 +33,17 @@ defmodule Urlpreview.Mixfile do
     [{:httpotion, "~> 3.0.2"}, # https://github.com/myfreeweb/httpotion
      {:floki, "~> 0.17.0"}, # https://github.com/philss/floki
      {:fuzzyurl, "~> 0.9.0"}] # https://github.com/gamache/fuzzyurl.ex
+  end
+
+  defp package do
+    %{
+      name: :urlpreview,
+      maintainers: ["Cameron Lockhart"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/cameeob2003/url-preview"
+      },
+      source_url: "https://github.com/cameeob2003/url-preview"
+    }
   end
 end
